debeb4383ee4d2d919c8bf502f866c4c8ba57835
ImJuandix <83036872+ImJuandix@users.noreply.github.com>
2022-02-21 22:41:13 -0500

    Patrones

---
+++ Book.java
@@ -0,0 +1,12 @@
+package com.youtube.geekific;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private final String name;
+    private final double price;
+    private final BookType type;
+
+}
